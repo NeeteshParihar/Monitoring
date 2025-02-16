4e00c240288c369416504457848247170c79802e
NeeteshParihar <neeteshparihar22@gmail.com>
2025-02-16 21:55:14 +0530

    push data to github by a nodejs script

---
+++ upload.js
@@ -0,0 +1,21 @@
+
+
+const {exec} = require("child_process") ;
+
+const intervalID = setInterval( ()=>{
+
+    const command = `git add . && git commit -m "push data to github by a nodejs script" && git push origin main` ;
+
+    exec( command  , ( error , stdout , stderr )=>{
+
+        if(error){
+            console.log(error.message) ;
+        }else if( stderr){
+            console.log(stderr);
+        }else{
+            console.log(stdout) ;
+        }
+
+    })
+
+} , 20000)
