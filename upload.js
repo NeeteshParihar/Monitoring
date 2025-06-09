3bf24c6bbe04ced5eadeec2f4fc7e316146f1fb3
Neetesh <neeteshparihar22@gmail.com>
2025-06-09 22:32:53 +0530

    push data to github by a nodejs script

--- upload.js
+++ upload.js
@@ -5,7 +5,7 @@ import {exec} from "child_process";
 
 const intervalID = setInterval( ()=>{
 
-    const command = `git add . && git commit -m "push data to github by a nodejs script" && git push --f origin main` ;
+    const command = `git add . && git commit -m "push data to github by a nodejs script" && git push origin main` ;
 
     exec( command  , ( error , stdout , stderr )=>{
 
